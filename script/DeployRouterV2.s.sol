856af05e89d78d1f37e3497754ae0fa7f6527a34
zexoverz <faisalfirdani01@gmail.com>
2026-02-06 16:45:14 +0700

    deploy GrimSwapRouterV2 at 0x5EE78E89A

--- script/DeployRouterV2.s.sol
+++ script/DeployRouterV2.s.sol
@@ -2,24 +2,12 @@
 pragma solidity ^0.8.26;
 
 import {Script, console} from "forge-std/Script.sol";
-import {GrimSwapRouterV2} from "../src/zk/GrimSwapRouterV2.sol";
-
-interface IGrimPoolMultiToken {
-    function releaseForSwap(uint256 amount) external;
-    function releaseTokenForSwap(address token, uint256 amount) external;
-    function setAuthorizedRouter(address router, bool authorized) external;
-}
-
-interface IPoolSwapTest {
-    struct TestSettings {
-        bool takeClaims;
-        bool settleUsingBurn;
-    }
-}
+import {GrimSwapRouterV2, IGrimPoolMultiToken, IPoolSwapTest} from "../src/zk/GrimSwapRouterV2.sol";
+import {GrimPoolMultiToken} from "../src/zk/GrimPoolMultiToken.sol";
 
 contract DeployRouterV2 is Script {
     // Existing contracts on Unichain Sepolia
-    address constant GRIM_POOL_MULTI_TOKEN = 0x6777cfe2A72669dA5a8087181e42CA3dB29e7710;
+    address payable constant GRIM_POOL_MULTI_TOKEN = payable(0x6777cfe2A72669dA5a8087181e42CA3dB29e7710);
     address constant SWAP_ROUTER = 0x9140a78c1A137c7fF1c151EC8231272aF78a99A4;
 
     function run() external {
@@ -35,7 +23,7 @@ contract DeployRouterV2 is Script {
         console.log("GrimSwapRouterV2 deployed at:", address(router));
 
         // Authorize the new router on GrimPoolMultiToken
-        IGrimPoolMultiToken(GRIM_POOL_MULTI_TOKEN).setAuthorizedRouter(address(router), true);
+        GrimPoolMultiToken(GRIM_POOL_MULTI_TOKEN).setAuthorizedRouter(address(router), true);
         console.log("Authorized router on GrimPoolMultiToken");
 
         vm.stopBroadcast();
